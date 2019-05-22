112745ad846f0ea3f948687f2ec5b3517a4966da
15j5141 <15j5141@stu.meisei-u.ac.jp>
2019-05-22 17:29:36 +0900

    [fix] preg_matchで十分.

--- sample.config.php
+++ sample.config.php
@@ -20,7 +20,7 @@ $JWConfig["rootURL"]='https://www.abc.xxx:8888/app/wiki/';
 /* - The following settings are automatic.
 But if it doesn't work please enter it manually. */
 $JWConfig["rootURL"]=$JWConfig["rootURL"] . (substr($JWConfig["rootURL"], -1)!='/'? '/': '');// slash check.
-preg_match_all("|^https?://([-\w\.]+)(:\d+)?/(.*)$|", $JWConfig["rootURL"], $out, PREG_PATTERN_ORDER);
+preg_match("|^https?://([-\w\.]+)(:\d+)?/(.*)$|", $JWConfig["rootURL"], $out, PREG_PATTERN_ORDER);
 $JWConfig["siteDomain"]=$out[1];//strtok($_SERVER['HTTP_HOST'],':');
 $JWConfig["sitePort"]=$out[2];//':'. $_SERVER['REMOTE_PORT']
 $JWConfig["siteDir"]=$out[3];
