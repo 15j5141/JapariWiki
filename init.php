c06263658a2ee702f7c371d3fcc8b79e94ea34ba
15j5141 <15j5141@stu.meisei-u.ac.jp>
2019-05-22 14:45:42 +0900

    [add] #7 アカウントテーブル作成用に init.phpを作成.

---
+++ init.php
@@ -0,0 +1,45 @@
+<?php
+/* php-init */
+header('Content-Type: text/html; charset=UTF-8');
+require_once(__DIR__.'/../../db.php');
+//require_once(__DIR__.'/lib/module_dataBase.php');
+?>
+
+
+<?php
+/* php-run */
+
+// アカウント用テーブルの作成.
+//$sql="DROP TABLE IF EXISTS app_account;";
+$sql="CREATE TABLE app_account (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  user_id varchar(16) NOT NULL,
+  user_name varchar(16) NOT NULL,
+  user_pass varchar(255) NOT NULL,
+  lastdate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+$result='';
+$mysqli = new mysqli($db['host'], $db['id'], $db['pass'], $db['dbname']);
+if ($mysqli->connect_error) {
+  $result = $mysqli->connect_error;
+}else{
+  $mysqli->set_charset("utf8");
+  $result = $mysqli->query($sql);
+  $mysqli->close();
+}
+
+?>
+
+<!-- html-css -->
+
+
+<!-- html-js -->
+
+
+<!-- html-body -->
+<html>
+<body>
+  <?= $result; ?>
+</body>
+</html>
