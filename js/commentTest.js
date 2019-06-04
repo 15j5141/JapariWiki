faa6a8f8c4082a85bcc3bd53a7bc0463c295b7a3
15j5141 <15j5141@stu.meisei-u.ac.jp>
2019-06-05 02:51:34 +0900

    [add] jsでコメント機能の実装途中.
    checkBeforeSavingPage = 編集画面でのサーバへの保存前チェック
    checkBSP_NewCommentForm= 新規コメント構文にID割り当て
    checkAfterLodingPage = ページ読み込み後の構文解析
    checkComment = コメント構文をコメントに差し替える
    getComment = コメント受信本体setComment = コメント登録本体
    rndStr = 指定文字数のランダムな文字列生成

--- js/commentTest.js
+++ js/commentTest.js
@@ -4,9 +4,60 @@ var ncmb = new NCMB(appKey, clientKey);
 var user = new ncmb.User();
 username = 'commenter';
 password = 'commenter';
+var currentUser = ncmb.User.getCurrentUser();
+if (currentUser) {
+  console.log("ログイン中のユーザー: " + currentUser.get("userName"));
+} else {
+  //ncmb.User.logout();
+  console.log("未ログインまたは取得に失敗");
+}
+ncmb.User.login(username, password)
+  .then(function(user) {
+    console.log('login: success!');
+    console.log(user);
+    return OnLogin();
+  }).then(function(out) {
+    console.log(out);
+  }).catch(err => console.log(err));
+
+function OnLogin() {
+  return new Promise((resolve, reject) => {
+    checkComment(txt, ncmb).then(function(result) {
+      $('body').append(result);
+      console.log(result);
+      return checkAfterLodingPage(result, ncmb);
+    }).then(function(result) {
+      resolve('checkAfterLodingPage: success');
+    }).then(function() {
+      resolve('all: success');
+    }).catch(function(err) {
+      reject(err);
+    });
+  });
+}
+$(function() {
+  txt = "fdgwhsdijfpv\n#comment()\nnobsgjprgnb";
+  /*checkBeforeSavingPage(txt, ncmb).then(function(result){
+    console.log(result);
+  });*/
+  txt = "start<br />\n#comment(8YxY37D1Il)<br />\n#comment(8YxY37D1I2)<br />\n#comment(8YxY37D1Il)<br />\nend<br />";
+  $('body').append('<br >loading...<br >');
+
+  /*
+    let user = new ncmb.User({
+      userName: 'commenter',
+      password: 'commenter'
+    });
+    ncmb.User.login(user).then(function(user) {
+      setComment(ncmb.DataStore("Comment"), '8YxY37D1I2', rndStr(24), 'commenter').then(function(x) {
+        console.log(x);
+      });
+    });
+  */
+});
+
 /* // TestClass
   var TestClass = ncmb.DataStore("TestClass");
-
   var testClass = new TestClass();
   testClass.set("message", "Hello, NCMB!");
   testClass.save()
@@ -64,34 +115,37 @@ user.set("userName", username)
     .catch(function(error) {
     });
 */
-ncmb.User.login(username, password)
-  .then(function(user) {
-    /*
-    var Comment = ncmb.DataStore("Comment");
-    var comment = new Comment();
-    comment.set("content", "コンテンツ")
-      .set("contributor", "19j5000")
-      .save()
-      .then(function(gameScore) {
-        console.log("zz1");
-      })
-      .catch(function(err) {
-        console.log("zz2");
-      });
-      */
-    var Comment = ncmb.DataStore("Comment");
-    Comment.equalTo("objectId", "8IiBSttkFYmyKG3G")
-      //.order("score", true)
-      .fetchAll()
-      .then(function(results) {
-        console.log("Successfully retrieved " + results.length + " scores.");
-        for (var i = 0; i < results.length; i++) {
-          var object = results[i];
-          console.log(object.content + " - " + object.get("contributor"));
-        }
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-  })
-  .catch(function(err) {});
+$(function() {
+  return;
+  ncmb.User.login(username, password)
+    .then(function(user) {
+      /*
+      var Comment = ncmb.DataStore("Comment");
+      var comment = new Comment();
+      comment.set("content", "コンテンツ")
+        .set("contributor", "19j5000")
+        .save()
+        .then(function(gameScore) {
+          console.log("zz1");
+        })
+        .catch(function(err) {
+          console.log("zz2");
+        });
+        */
+      var Comment = ncmb.DataStore("Comment");
+      Comment.equalTo("objectId", "8IiBSttkFYmyKG3G")
+        //.order("score", true)
+        .fetchAll().then(function(results) {
+          console.log("Successfully retrieved " + results.length + " scores.");
+          for (var i = 0; i < results.length; i++) {
+            var object = results[i];
+            console.log(object.content + " - " + object.get("contributor"));
+          }
+        }).catch(function(err) {
+          console.log(err);
+        });
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+});
---
+++ js/text2html.js
@@ -0,0 +1,134 @@
+// require ncmb.min.js
+function checkBeforeSavingPage(rawText, ncmb) { // 編集後の保存前の確認.
+  return new Promise(function(resolve, reject) {
+    var resultText = rawText;
+    // 新規コメント文法検出.(1個だけ)
+    // fixme 同時に複数の新規コメントフォーム設置
+    // fixme コメントidの重複確認
+    checkBSP_NewCommentForm(resultText).then(
+      result => {
+        return checkBSP_NewCommentForm(result);
+      }
+    ).then(result => {
+      resolve(result);
+    }).catch(err => {
+      console.log(err);
+      resolve(err);
+    });
+  });
+}
+// 新しい#comment(null)に適当な米idを割り当てる.Promiseじゃなくていい
+function checkBSP_NewCommentForm(rawText) {
+  return new Promise(function(resolve, reject) {
+    var resultText = rawText;
+    if (/#comment\(\s*\)/.test(rawText)) {
+      resultText = resultText.replace(/#comment\(\s*\)/g, function(s) {
+        var comObj = rndStr(10);
+        return '#comment(' + comObj + ')';
+      });
+    }
+    resolve(resultText);
+  });
+}
+
+function checkAfterLodingPage(rawText, ncmb) { // 読込後の構文.
+  return new Promise(function(resolve, reject) {
+    let result = rawText;
+    checkALP_Comment(result);
+    checkComment(result, ncmb).then(function(result) {
+      console.log('ok');
+    }).then(function(result) {
+      resolve(result);
+    });
+  });
+}
+
+function checkALP_Comment(text) {
+  let regexp = /#comment\(([a-zA-Z0-9]{6,10})\)/g;
+  return text;
+}
+
+
+function checkComment(rawText, ncmb) { // コメントフォームの差し替え
+  return new Promise(function(resolve, reject) {
+    // 初期化
+    var resultText = rawText;
+    var regexp = /#comment\(([a-zA-Z0-9]{6,10})\)/g;
+    // commenterでログイン.
+    let user = new ncmb.User({
+      userName: 'commenter',
+      password: 'commenter'
+    });
+    //user
+    //ncmb.User.login(user).then(function(user) {
+    var Comment = ncmb.DataStore("Comment");
+    var comment = new Comment();
+    let match;
+    let ids = [];
+    // コメントのobjIDのみ取り出し
+    while ((match = regexp.exec(resultText)) !== null) {
+      ids.push(getComment(Comment, match[1]));
+    }
+    //return
+    Promise.all(ids).then(function(cforms) { //
+      console.info('all fullfilled, v cforms v');
+      console.log(cforms);
+      // 構文置換
+      for (var i = 0; i < cforms.length; i++) {
+        resultText = resultText.replace('#comment(' + cforms[i][0].commentObjectId + ')', cforms[i].map(v => v.content));
+      }
+      //return user.logout();
+    }).then(function() {
+      resolve(resultText);
+    }).catch(function(err) {
+      console.log(err);
+      reject(err);
+    });
+  });
+}
+// コメント受信
+function getComment(commentClass, id) {
+  return new Promise((resolve, reject) => {
+    // Commentデータストアに接続.
+    commentClass.equalTo("commentObjectId", id)
+      .order("createDate", true)
+      .fetchAll() // 受信
+      .then(function(results) {
+        console.log("Successfully retrieved " + results.length + " scores.");
+        resolve(results);
+      })
+      .catch(function(err) {
+        console.log(err);
+        reject(err);
+      });
+  });
+}
+
+function setComment(CommentClass, comObj, content, contributor) {
+  return new Promise((resolve, reject) => {
+    //var Comment = ncmb.DataStore("Comment");
+    var comment = new CommentClass();
+    comment
+      .set("commentObjectId", comObj) // コメントID
+      .set("content", content) // 内容
+      .set("contributor", contributor) // 投稿者
+      .save() // データストアに接続.
+      .then(function(obj) {
+        resolve(obj)
+      })
+      .catch(function(err) {
+        reject(err);
+      });
+  });
+}
+// lengthの長さのランダムな文字列を生成
+function rndStr(length) {
+  var moji = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var result = '';
+  var i = 0;
+  while (i < length) {
+    result += moji[Math.floor(Math.random() * moji.length)];
+    i++;
+  }
+  return result;
+}
