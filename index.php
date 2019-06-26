9f771647bf1d5c7c7d89511c2a74b88d39ae27f0
15j5141 <15j5141@stu.meisei-u.ac.jp>
2019-06-27 02:35:44 +0900

    [update] #12 一応関数定義ファイルを先にロードさせる.

--- index.php
+++ index.php
@@ -33,8 +33,8 @@ $page = isset($_SESSION['page']) ? $_SESSION['page'] : 'FrontPage';
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script type="text/javascript" src="lib/ncmb.min.js" charset="utf-8">
   </script>
-  <script type="text/javascript" src="js/modAjaxWikiBase.js"></script>
   <script type="text/javascript" src="js/text2html.js"></script>
+  <script type="text/javascript" src="js/modAjaxWikiBase.js"></script>
   <script type="text/javascript">
 
     $(function(){
