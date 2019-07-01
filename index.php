e390eb0fd20c0374de21400e7b638b49cea39a96
15j5141 <15j5141@stu.meisei-u.ac.jp>
2019-07-01 23:49:31 +0900

    [update] sideからtwitterやdiscordのiframeを削除.

--- index.php
+++ index.php
@@ -83,15 +83,6 @@ $page = isset($_SESSION['page']) ? $_SESSION['page'] : 'FrontPage';
       History<br />
       <?php echo loadText("./text/site_History.txt"); ?>
     </div>
-    <div id='side_discord'>
-      <iframe src="https://discordapp.com/widget?id=xxxxxxxx&amp;theme=dark" width="250" height="300" allowtransparency="true" frameborder="0"></iframe>
-    </div>
-    <div id='side_twitter'>
-      <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/">
-        Tweets by ___
-      </a>
-      <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-    </div>
   </div>
   <div id="footer">
     Loading Now...<br />
