3f68a9388c0e91a67df304a29f761c741590cb45
15j5141 <15j5141@stu.meisei-u.ac.jp>
2019-12-25 19:39:26 +0900

    URLQuery の削除処理を修正. URLQuery セット時 historyAPI で URL 置換する方式に変更.

--- js/class-util.js
+++ js/class-util.js
@@ -11,6 +11,7 @@ class JWUtil {
         const query = {};
         location.search.substring(1).split('&').forEach(q => {
             const obj = q.split('=');
+            if (obj.length !== 2) { return; }
             query[obj[0]] = obj[1];
         });
         return query;
@@ -21,19 +22,22 @@ class JWUtil {
     static set URLQuery(query) {
         // URLからクエリを抽出.
         const result = Object.keys(query).map(key => key + '=' + query[key]).join('&');
-        location.search = '?' + result;
+        // URLを構築.
+        const url = location.origin + location.pathname + '?' + result + location.hash;
+        // URLを置換.
+        history.replaceState(null, '', url);
     }
     static setURLQuery(key, value) {
-        const obj = Util.URLQuery;
-        if (value === null) {
+        const obj = JWUtil.URLQuery;
+        if (value == null) { // undefinedでもtrue.
             delete obj[key];
         } else {
             obj[key] = value;
         }
-        Util.URLQuery = obj;
+        JWUtil.URLQuery = obj;
     }
     static getURLQuery(key) {
-        return Util.URLQuery[key];
+        return JWUtil.URLQuery[key];
     }
 }
 export default JWUtil;
