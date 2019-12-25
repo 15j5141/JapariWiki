6a83ba87679e90e4cbe3f58c636ca36052f12a89
15j5141 <15j5141@stu.meisei-u.ac.jp>
2019-12-25 21:10:33 +0900

    babel と json のローダーを追加.

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,38 @@ module.exports = {
 			template:'temp.html',
 			filename: 'app/sub.html'
 		})
-	]
+	],
+	externals: [
+		{
+			jquery: 'jQuery',
+			ncmb: 'NCMB'
+		}
+	],
+	module: {
+		rules: [
+			{
+				test: /\.sjs$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							[
+								"@babel/preset-env",
+								{
+									useBuiltIns: "usage",
+									corejs: 3 // or 2
+								}
+							]
+						]
+					}
+				}
+			},
+			{
+				// npm install json-loader --save-dev
+				test: /\.json$/,
+				loader: 'json'
+			},
+		]
+	},
 }
