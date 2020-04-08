ca951b916db7c68d77e8732cddbdad54e25d7c10
15j5141 <15j5141@stu.meisei-u.ac.jp>
2020-04-08 16:25:03 +0900

    フラグ変数の型を int から bool に変更.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
-var doneAjax = 1;
-var isCanBeHistory = history && history.pushState && history.state !== undefined;
 
-$(function () {
+let doneAjax = true;
+const isCanBeHistory = history && history.pushState && history.state !== undefined;
+
+$(function() {
 
   // <a class="ajaxLoad">をクリックしたら. Wiki内ページリンクを踏んだら.
-  $(document).on('click', 'a.ajaxLoad', function (event) {
+  $(document).on('click', 'a.ajaxLoad', function(event) {
     event.preventDefault(); // 標準ページ移動を無効化.
     /* 連打対策 */
-    if (doneAjax == 1) {
-      doneAjax = 0;
+    if (doneAjax) {
+      doneAjax = false;
 
       $('#content_add').attr("data-ajax", "load"); // 現在のajax画面はload.
       page = $(this).attr('value'); // <a value=xxx>を取得.
@@ -36,30 +37,30 @@ $(function () {
               $('#content_add').trigger('rewrite'); // 発火させる
             }
             $("#content_add").fadeIn('1'); // ページを表示する.
-            doneAjax = 1;
+            doneAjax = true;
           });
         },
         // エラー時処理.
         error: function () {
           $("#content_add").html('Ajax通信エラーです。');
           $("#content_add").fadeIn('1'); // ページを表示する.
-          doneAjax = 1;
-        }
-      }); /* $.ajax */
+          doneAjax = true;
+        });
+
     } /* if */
     history.pushState("" + page, null, null); // 遷移を履歴に追加.
     return false; // <a>を無効化.
   });
 
   // 「編集」ボタンを押したら.
-  $(document).on('click', '#ajaxLoad_edit', function (event) {
+  $(document).on('click', '#ajaxLoad_edit', function(event) {
     event.preventDefault();
     ajaxLoad("#content_add", "ajax_edit.php?page=" + encodeURIComponent(page));
     return false;
   });
 
   // 「アップロード」ボタンを押したら.
-  $(document).on('click', '#ajaxLoad_upload', function (event) {
+  $(document).on('click', '#ajaxLoad_upload', function(event) {
     event.preventDefault();
     // キーボード操作などにより、オーバーレイが多重起動するのを防止する
     $(this).blur(); //ボタンからフォーカスを外す
@@ -76,10 +77,10 @@ $(function () {
     return false;
   });
 
-  $(document).on('click', '#modal-overlay', function (event) {
+  $(document).on('click', '#modal-overlay', function(event) {
     // [#modal-overlay]と[#modal-close]をフェードアウトする
     if ($(event.target).is("#modal-overlay")) {
-      $("#modal-close,#modal-overlay").fadeOut("slow", function () {
+      $("#modal-close,#modal-overlay").fadeOut("slow", function() {
         // フェードアウト後、[#modal-overlay]をHTML(DOM)上から削除
         $("#modal-overlay").remove();
       });
@@ -87,7 +88,7 @@ $(function () {
   });
 
   // ページバック処理時にページ遷移を発動.
-  $(window).on('popstate', function (e) {
+  $(window).on('popstate', function(e) {
     if (isCanBeHistory) {
       ajaxLoad("#content_add", "ajax_load.php?page=" + encodeURIComponent(history.state), () => $("#content_add").trigger('rewrite'));
     }
@@ -101,7 +102,7 @@ $(function () {
     history.replaceState('' + page, null, null);
   }
 
-  $(document).on('click', '#ajaxLoad_kanban', function (event) {
+  $(document).on('click', '#ajaxLoad_kanban', function(event) {
     event.preventDefault();
     ajaxLoad("#content_add", "ajax_kanban.php");
     return false;
@@ -114,7 +115,7 @@ function ajaxLoad(Content, Url, callbackSuccess) {
   $.ajax({
     url: "" + Url,
     cache: false,
-    success: function (html) {
+    success: function(html) {
       $(Content).html(html);
       if (typeof callbackSuccess === 'function') callbackSuccess(html);
     }
