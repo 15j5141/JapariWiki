526765dfffbd89d3f364a7ca29964f17421c9744
15j5141 <15j5141@stu.meisei-u.ac.jp>
2020-04-13 07:59:46 +0900

    NCMB でのサインイン, サインアウト, サインアップの実装.

--- js/class-cloud_ncmb.js
+++ js/class-cloud_ncmb.js
@@ -7,10 +7,29 @@ import JWPage from './class-page.js';
  */
 class CloudNCMB extends CloudBase {
   /** @override */
-  constructor() {}
-  /** @override */
-  getPage(pageName) {
-    throw new Error('Not Implemented');
+  constructor() {
+    super();
+    const ncmb = {
+      name: 'ncmb',
+      appKey:
+        '73214ee5293b310aba334aaf6b58cb41cb89873a1eb88ab505fa7d48dcc2b911',
+      clientKey:
+        '79d3a32839ef780c0fe16236d8efc7bdd338312ec1d969945346a181ffc9442f',
+      instance: null,
+    };
+    this.appKey = ncmb.appKey || '';
+    this.clientKey = ncmb.clientKey || '';
+    this.instance = this;
+    this.ncmb = new window.NCMB(this.appKey, this.clientKey);
+    console.log(this.ncmb);
+
+    // カレントユーザー情報の取得
+    const currentUser = this.ncmb.User.getCurrentUser();
+    if (currentUser) {
+      console.log('ログイン中のユーザー: ' + currentUser.get('userName'));
+    } else {
+      console.log('未ログインまたは取得に失敗');
+    }
   }
   /** @override */
   putPage(pageURI, pageData) {
@@ -26,19 +45,40 @@ class CloudNCMB extends CloudBase {
   }
   /** @override */
   isLogin() {
-    throw new Error('Not Implemented');
+    // カレントユーザー情報の取得
+    const currentUser = this.ncmb.User.getCurrentUser();
+    return Boolean(currentUser);
   }
   /** @override */
-  signIn(id, pass) {
-    throw new Error('Not Implemented');
+  async signIn(id, pass) {
+    // ログイン用インスタンス作成.
+    const user = new this.ncmb.User({
+      userName: id,
+      password: pass,
+    });
+    // 非同期でログイン処理.
+    try {
+      return await this.ncmb.User.login(user);
+    } catch (err) {
+      console.log(err);
+      throw err; // エラー.
+    }
   }
   /** @override */
-  signUp(id, pass) {
-    throw new Error('Not Implemented');
+  async signUp(id, pass) {
+    // Userインスタンスの生成
+    const user = new ncmb.User();
+    user.set('userName', id).set('password', pass);
+    // 新規登録処理
+    return await user.signUpByAccount().catch(function(err) {
+      // エラー処理.
+      throw err;
+    });
   }
   /** @override */
-  signOut() {
-    throw new Error('Not Implemented');
+  async signOut() {
+    return await this.ncmb.User.logout();
+    // message: "This user doesn't login."
   }
 }
 export default CloudNCMB;
