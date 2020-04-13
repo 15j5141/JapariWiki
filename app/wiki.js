d83e64ebd4238eae5afe9c948b44e2b46593b0b8
15j5141 <29105288+15j5141@users.noreply.github.com>
2020-04-13 21:54:34 +0900

    CRLF2LF.

--- app/wiki.js
+++ app/wiki.js
@@ -1,36 +1,39 @@
 // import * as convertHTML from "js/module_convertHTML.js";
-import JWStatus from "../js/jw-status";
+import JWStatus from '../js/jw-status';
 // import { NCMB } from "ncmb";
 import $ from 'jquery';
 
-let status = new JWStatus();
-
+const status = new JWStatus();
 
 console.log(NCMB);
 console.log($);
 console.log(jQuery);
 
-let page = status.getPageName();
+const page = status.getPageName();
 
 // ニフクラインスタンス生成.
 const ncmb = new NCMB(JW.cloud.appKey, JW.cloud.clientKey);
 
 const Page = ncmb.DataStore('Page');
 // 受信.
-Page.equalTo('path', page).fetch()
-    .then(function(result) {
-        console.log(result.text);
-        $('#app_wiki-body').html(result.text);
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
+Page.equalTo('path', page)
+  .fetch()
+  .then(function(result) {
+    console.log(result.text);
+    $('#app_wiki-body').html(result.text);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
 // URLからクエリを抽出.
-let params = {};
-location.search.substring(1).split('&').forEach(q => {
+const params = {};
+location.search
+  .substring(1)
+  .split('&')
+  .forEach(q => {
     params[q.split('=')[0]] = q.split('=')[1];
-});
+  });
 console.log(params);
 
 // convertHTML.loadText(params['page']);
@@ -53,7 +56,6 @@ ex https://aaa.bbb/ccc?d=e&f=g
 //     $page = 'FrontPage';
 //   }
 //   echo loadText("text/" . $page . ".txt");
-
 
 //   $_SESSION['page']=$page;
 // }elseif(isset($_POST['text'])){
