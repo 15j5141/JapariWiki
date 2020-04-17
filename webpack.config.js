a002076bce8e6243ee21859b22c77f967eaec0d9
15j5141 <29105288+15j5141@users.noreply.github.com>
2020-04-18 00:22:37 +0900

    [clean] webpack 周りの設定調整. ESLint.

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,60 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: process.env.NODE_ENV || 'development',
-	entry: {
-		entry: './entry.js',
-		wiki: './app/wiki.js'
-	},
-	output: {
-		path: __dirname + '/dist',
-		filename: 'app/[name].js'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			// inject: false,
-			chunks: ['entry'],
-			filename: 'app/index.html'
-		}),
-		new HtmlWebpackPlugin({
-			// inject: false,
-			chunks: ['wiki'],
-			template: 'app/wiki.html',
-			filename: 'app/wiki.html'
-		})
-	],
-	externals: [
-		{
-			jquery: 'jQuery',
-			ncmb: 'NCMB'
-		}
-	],
-	module: {
-		rules: [
-			{
-				test: /\.sjs$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: [
-							[
-								"@babel/preset-env",
-								{
-									useBuiltIns: "usage",
-									corejs: 3 // or 2
-								}
-							]
-						]
-					}
-				}
-			},
-			{
-				// npm install json-loader --save-dev
-				test: /\.json$/,
-				loader: 'json'
-			},
-		]
-	},
-}
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    index: './js/index.js',
+    wiki: './app/wiki.js',
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'app/[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // inject: false,
+      chunks: ['index'],
+      template: 'app/index.html',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      // inject: false,
+      chunks: ['wiki'],
+      template: 'app/wiki.html',
+      filename: 'app/wiki.html',
+    }),
+  ],
+  externals: [
+    {
+      jquery: 'jQuery',
+      ncmb: 'NCMB',
+    },
+  ],
+  module: {
+    rules: [
+      {
+        test: /\._js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'usage',
+                  corejs: 3, // or 2
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        // npm install json-loader --save-dev
+        test: /\.json$/,
+        loader: 'json',
+      },
+    ],
+  },
+};
