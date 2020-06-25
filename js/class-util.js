799ccd1cd0d215192f47767da4213c5aa3b6fad1
15j5141 <29105288+15j5141@users.noreply.github.com>
2020-06-26 04:09:49 +0900

    [clean] コードをフォーマット.

--- js/class-util.js
+++ js/class-util.js
@@ -3,41 +3,55 @@
  * @class
  */
 class JWUtil {
-    /**
-     * @returns {Object}
-     */
-    static get URLQuery() {
-        // URLからクエリを抽出.
-        const query = {};
-        location.search.substring(1).split('&').forEach(q => {
-            const obj = q.split('=');
-            if (obj.length !== 2) { return; }
-            query[obj[0]] = obj[1];
-        });
-        return query;
-    }
-    /**
-     * @param {Object} query
-     */
-    static set URLQuery(query) {
-        // URLからクエリを抽出.
-        const result = Object.keys(query).map(key => key + '=' + query[key]).join('&');
-        // URLを構築.
-        const url = location.origin + location.pathname + '?' + result + location.hash;
-        // URLを置換.
-        history.replaceState(null, '', url);
-    }
-    static setURLQuery(key, value) {
-        const obj = JWUtil.URLQuery;
-        if (value == null) { // undefinedでもtrue.
-            delete obj[key];
-        } else {
-            obj[key] = value;
+  /**
+   * @return {Object}
+   */
+  static get URLQuery() {
+    // URLからクエリを抽出.
+    const query = {};
+    location.search
+      .substring(1)
+      .split('&')
+      .forEach(q => {
+        const obj = q.split('=');
+        if (obj.length !== 2) {
+          return;
         }
-        JWUtil.URLQuery = obj;
+        query[obj[0]] = obj[1];
+      });
+    return query;
+  }
+  /**
+   * @param {Object} query
+   */
+  static set URLQuery(query) {
+    // URLからクエリを抽出.
+    const result = Object.keys(query)
+      .map(key => key + '=' + query[key])
+      .join('&');
+    // URLを構築.
+    const url =
+      location.origin + location.pathname + '?' + result + location.hash;
+    // URLを置換.
+    history.replaceState(null, '', url);
+  }
+  /**
+   *
+   * @param {*} key
+   * @param {*} value
+   */
+  static setURLQuery(key, value) {
+    const obj = JWUtil.URLQuery;
+    if (value == null) {
+      // undefinedでもtrue.
+      delete obj[key];
+    } else {
+      obj[key] = value;
     }
-    static getURLQuery(key) {
-        return JWUtil.URLQuery[key];
-    }
+    JWUtil.URLQuery = obj;
+  }
+  static getURLQuery(key) {
+    return JWUtil.URLQuery[key];
+  }
 }
 export default JWUtil;
