9b30f248e52d03b4a5388c5f554fd7145e9ed177
15j5141 <29105288+15j5141@users.noreply.github.com>
2020-09-02 00:57:50 +0900

    [update] サービスを参照するように変更.

--- src/app/history.component.js
+++ src/app/history.component.js
@@ -1,6 +1,8 @@
 // @ts-check
 import CloudNCMB from '../scripts/class-cloud_ncmb.js';
 import ComponentBase from '../scripts/class-component_base.js';
+import { StatusService } from './status.service.js';
+import ApplicationService from './application.service.js';
 
 /**
  * @class
@@ -9,7 +11,16 @@ export default class HistoryComponent extends ComponentBase {
   /**
    * @override
    */
-  decorator() {}
+  decorator() {
+    /* ----- サービスのインジェクション. ----- */
+    /** @type {{status: StatusService, application: ApplicationService}} */
+    this.serviceInjection = {
+      status: StatusService.prototype,
+      application: ApplicationService.prototype,
+    };
+
+    /* ----- プロパティ宣言. ----- */
+  }
   /**
    * @override
    */
@@ -25,6 +36,12 @@ export default class HistoryComponent extends ComponentBase {
    * @override
    */
   async onInit() {
-    this._cloud = new CloudNCMB();
+    this.status = this.serviceInjection.status;
   }
+  /** @override */
+  async onRender() {
+    const cloud = this.status.getCloud();
+  }
+  /** @override */
+  onLoad() {}
 }
