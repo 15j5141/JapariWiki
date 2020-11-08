0d80c6d7f6e77aa6692991a2b1c1b7682aeda983
15j5141 <29105288+15j5141@users.noreply.github.com>
2020-11-09 08:16:12 +0900

    [add] ページ名 /components/ からコンポーネントを検索して読み込むようにする.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -56,6 +56,11 @@ export class AppComponent extends ComponentBase {
     if (user == null) location.href = './logout.html';
     status.setUser(user);
 
+    // 動的コンポーネント一覧を取得する.
+    const paths = await this.getDynamicComponentPaths();
+    // 動的コンポーネントを取得する.
+    await this.loadDynamicComponents(paths);
+
     // 描画する.
     this.renderer.setHTML(await this.templateHTML);
   }
@@ -92,6 +97,76 @@ export class AppComponent extends ComponentBase {
           });
         }
       });
+    });
+  }
+  /**
+   * @param {any} paths
+   */
+  async loadDynamicComponents(paths) {
+    const self = this;
+
+    // パスを通す.
+    window.requirejs.config({
+      paths,
+    });
+
+    // 動的コンポーネントクラスを読込開始する.
+    const pathsPromise = Promise.all(
+      Object.keys(paths).map(path => {
+        return new Promise((resolve, reject) => {
+          // RequireJS でコンポーネントを読み込む.
+          window.requirejs([path], resolve, reject);
+        }).catch(err => {
+          // 読込失敗してもnullを返して他の読み込みを邪魔させない.
+          console.log(err);
+          return null;
+        });
+      })
+    );
+
+    // 読込完了したらコンポーネントクラス一覧に追加する.
+    await pathsPromise.then(classes => {
+      classes.forEach(clazz => {
+        if (!clazz) return; // nullなら無視する.
+        // 追加する.
+        self.refObj.declarations.push(clazz);
+        ComponentBase.classes$.next(clazz);
+        // ログ出力する.
+        console.log('loaded: ' + clazz.name);
+      });
+    });
+  }
+  /**
+   * {{scriptName:URL},...} の形でコンポーネントのリンクを返す.
+   * @return {Promise<any>}
+   */
+  async getDynamicComponentPaths() {
+    const ncmb = this.serviceInjection.status.getCloud().ncmb;
+    const query = ncmb.DataStore('Page').order('createDate', true);
+
+    // 部分一致で検索できるようにクエリを微調整する.
+    query._where = {
+      path: { $regex: '^/components/[-a-zA-Z0-9_]*\\.component$' },
+    };
+
+    // 取得する.
+    return await query.fetchAll().then(function(componentObjects) {
+      // {{scriptName:URL},...}の形にして返す.
+      return componentObjects.reduce((paths, componentObject) => {
+        if (!componentObject.text) return paths;
+        // コンポーネントのコードをURLに変換する.
+        const url = window.URL.createObjectURL(
+          new Blob([componentObject.text], {
+            type: 'application/javascript',
+          })
+        );
+        // ファイル名を取得する.
+        const uris = componentObject.path.split('/');
+        const name = uris[uris.length - 1];
+        // 格納する.
+        paths[name] = url;
+        return paths;
+      }, {});
     });
   }
 }
